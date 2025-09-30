7d156382bc0e212722c66e391296ca7944acce7a
Matthew Schmidt <mschmidt@gatewayscripts.com>
2025-09-30 22:21:30 +0000

    Update AssemblyVersion to 1.0.0.2
    The `AssemblyVersion` attribute in `BeamNameApprExt.cs` was
    updated from `1.0.0.1` to `1.0.0.2`. This version increment
    reflects a minor update or bug fix. No changes were made to
    `AssemblyFileVersion` or `AssemblyInformationalVersion`.

--- BeamNameApprExt.cs
+++ BeamNameApprExt.cs
@@ -8,7 +8,7 @@ using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
 
 // TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
-[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
 [assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: AssemblyInformationalVersion("1.0")]
 
